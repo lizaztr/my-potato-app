de2256b8dd6b75294789cfeeb274a6f07e7a354c
Liza Torstensson <liza.torstensson@viaplaygroup.com>
2024-07-04 17:42:05 +0200

    merge

---
+++ build/precache-manifest.003ab9168222f6e5470dd743448f3549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003aaed98909fa71d3c9b26a761a9edd",
+    "url": "/my-potato-app/index.html"
+  },
+  {
+    "revision": "70b6dea6f803cb7ed323",
+    "url": "/my-potato-app/static/css/main.c53e2655.chunk.css"
+  },
+  {
+    "revision": "9c91d93369fbb8f38a3f",
+    "url": "/my-potato-app/static/js/2.ee318f2e.chunk.js"
+  },
+  {
+    "revision": "d672b6ca923d296642a21632e2130961",
+    "url": "/my-potato-app/static/js/2.ee318f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b6dea6f803cb7ed323",
+    "url": "/my-potato-app/static/js/main.5e83e951.chunk.js"
+  },
+  {
+    "revision": "c318981660a1a7b568c6",
+    "url": "/my-potato-app/static/js/runtime-main.c7a39885.js"
+  }
+]);
